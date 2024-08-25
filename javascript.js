f32d51801348d9fce5ed763ae68d671ca2425334
theokyle <121832097+theokyle@users.noreply.github.com>
2024-08-24 23:37:35 -0500

    Finished the playRound function

--- javascript.js
+++ javascript.js
@@ -14,9 +14,70 @@ function getHumanChoice() {
     return prompt("Rock, Paper, or Scissors?");
 }
 
-let compChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
 
-console.log(humanChoice);
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase()=="rock") {
+        switch (computerChoice) {
+            case "Rock":
+                console.log("Tie Game");
+                break;
+            case "Paper":
+                console.log("You Lose! Paper beats Rock");
+                computerScore += 1;
+                return;
+            case "Scissors":
+                console.log("You Win! Rock beats Scissors");
+                humanScore += 1;
+                return;
+        }
+    }
+    else if (humanChoice.toLowerCase()=="paper") {
+        switch (computerChoice) {
+            case "Paper":
+                console.log("Tie Game");
+                break;
+            case "Scissors":
+                console.log("You Lose! Scissors beats Paper");
+                computerScore += 1;
+                return;
+            case "Rock":
+                console.log("You Win! Paper beats Rock");
+                humanScore += 1;
+                return;
+        }
+    }
+    else if (humanChoice.toLowerCase()=="scissors") {
+        switch (computerChoice) {
+            case "Scissors":
+                console.log("Tie Game");
+                break;
+            case "Rock":
+                console.log("You Lose! Rock beats Scissors");
+                computerScore += 1;
+                return;
+            case "Paper":
+                console.log("You Win! Scissors beats Paper");
+                humanScore += 1;
+                return;
+        }
+    }
+    else {
+        console.log("Invalid choice, try again!");
+        return;
+    }
+}
+
+function playGame() {
+    
+}
+playRound(humanChoice, computerChoice);
+
+console.log(humanScore);
+console.log(computerScore);
 
 
